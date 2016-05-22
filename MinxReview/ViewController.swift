01e2f025705bab7c2364fa16b4b8bece93b0f38d
folklove45 <folklove45@hotmail.com>
2016-05-22 14:37:58 +0700

    Initial Commit

---
+++ MinxReview/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MinxReview
+//
+//  Created by FolkSonG on 5/22/2559 BE.
+//  Copyright © 2559 FolkSonG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
